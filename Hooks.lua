39036098063cf97119389f93cbf0e0feb5f96861
walterblack-lab <1akosbagan@gmail.com>
2026-02-11 17:14:00 +0100

    Update Hooks.lua

--- Hooks.lua
+++ Hooks.lua
@@ -1,10 +1,12 @@
--- Hooks.lua | Matrix Hub - Debug & Logger
+-- Hooks.lua | Matrix Hub - Debug Spy
 local Hooks = {}
 local LP = game:GetService("Players").LocalPlayer
 local oldNamecall
 
 function Hooks.Init()
     local mt = getrawmetatable(game)
+    if not mt then warn("Metatable nem elérhető!") return end
+    
     setreadonly(mt, false)
     oldNamecall = mt.__namecall
 
@@ -12,55 +14,31 @@ function Hooks.Init()
         local method = getnamecallmethod()
         local args = {...}
 
-        -- Csak akkor figyelünk, ha a játék hív meg valamit (nem a scriptünk)
         if not checkcaller() and method == "FireServer" then
-            
-            -- Ez a rész "szaglássza" ki a lövéseket
-            -- Megnézzük a Remote nevét vagy az adatok típusát
-            local isPossibleShot = false
-            if self.Name:lower():find("shoot") or self.Name:lower():find("fire") or self.Name:lower():find("bullet") then
-                isPossibleShot = true
-            end
-
-            -- Ha találtunk valamit, kiírjuk a konzolba (F9)
-            if isPossibleShot then
-                print("--------------------------------------")
-                print("LÖVÉS DETEKTÁLVA!")
-                print("Remote neve: " .. self.Name)
-                
-                -- Végigmegyünk az adatokon, amiket a fegyver küld
+            -- Megnézzük, lövés-e
+            local n = self.Name:lower()
+            if n:find("shoot") or n:find("fire") or n:find("bullet") or n:find("hit") then
+                print(">>> LÖVÉS DETEKTÁLVA: " .. self.Name)
                 for i, v in pairs(args) do
-                    print(string.format("Argumentum [%d] (%s): %s", i, typeof(v), tostring(v)))
-                    
-                    -- Ha a fegyver egy testrészt küld (pl. "Head" vagy "Torso")
-                    if typeof(v) == "Instance" then
-                        print("Eltalált objektum neve: " .. v.Name)
-                    end
+                    print(string.format("Arg[%d]: %s (%s)", i, tostring(v), typeof(v)))
                 end
-                print("--------------------------------------")
-            end
 
-            -- SILENT AIM RÉSZ (Módosítás teszteléshez)
-            if _G.SilentAimEnabled and isPossibleShot then
-                local Math = getgenv().MathUtils
-                local target = Math and Math.GetClosestTarget()
-                if target then
-                    -- Megpróbáljuk felülírni a Vector3 (pozíció) adatokat
-                    for i, v in pairs(args) do
-                        if typeof(v) == "Vector3" then
-                            args[i] = target.Position
+                if _G.SilentAimEnabled then
+                    local Math = getgenv().MathUtils
+                    local target = Math and Math.GetClosestTarget()
+                    if target then
+                        -- Koordináta hamisítás
+                        for i, v in pairs(args) do
+                            if typeof(v) == "Vector3" then args[i] = target.Position end
                         end
+                        return oldNamecall(self, unpack(args))
                     end
-                    return oldNamecall(self, unpack(args))
                 end
             end
         end
-
         return oldNamecall(self, ...)
     end)
-
     setreadonly(mt, true)
-    print("Matrix Logger Hooks aktív! Lőj egyet és nézd az F9-et!")
 end
 
 function Hooks.Disable()
